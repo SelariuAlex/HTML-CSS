1703db45b5abadffef7c030b335e6abd9ac96422
Selariu Alexandru <selalex_GD@yahoo.com>
2024-09-18 11:19:05 +0300

    if

---
+++ if.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    for (let i = 0; i < 2; i++) {
+        if (true) {
+            console.log("1 loop");
+        }
+
+        if (true) {
+            console.log("2 loop");
+        }
+
+        if (true) {
+            console.log("3 loop");
+
+        }
+
+        console.log('4 if')
+    }
+
+    console.log("5 outside");
+}
+
+myFunction();
